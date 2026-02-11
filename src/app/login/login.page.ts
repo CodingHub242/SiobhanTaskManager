13c2c2780752ea4eb6a30285f6e79aea015c4649
Jonathan Attram <codinghub242@gmail.com>
2026-02-11 00:29:31 +0000

    logt

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { IonicModule, NavController, ToastController } from '@ionic/angular';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -11,10 +13,57 @@ import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/stan
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class LoginPage implements OnInit {
+email: string = '';
+  password: string = '';
+  isLoading: boolean = false;
 
-  constructor() { }
+  constructor( private authService: AuthService,
+    private navController: NavController,
+    private toastController: ToastController) { }
 
   ngOnInit() {
+  }
+
+   async login(): Promise<void> {
+    if (!this.email || !this.password) {
+      this.showToast('Please enter email and password');
+      return;
+    }
+
+    this.isLoading = true;
+
+    this.authService.login({ email: this.email, password: this.password })
+      .subscribe({
+        next: () => {
+          this.isLoading = false;
+          this.navigateToDashboard();
+        },
+        error: (error) => {
+          this.isLoading = false;
+          this.showToast('Invalid credentials');
+        }
+      });
+  }
+
+  navigateToRegister(): void {
+    this.navController.navigateForward('/register');
+  }
+
+    private navigateToDashboard(): void {
+    if (this.authService.isAdmin()) {
+      this.navController.navigateRoot('/admin-dashboard');
+    } else {
+      this.navController.navigateRoot('/employee-dashboard');
+    }
+  }
+
+  private async showToast(message: string): Promise<void> {
+    const toast = await this.toastController.create({
+      message,
+      duration: 2000,
+      color: 'danger'
+    });
+    await toast.present();
   }
 
 }
