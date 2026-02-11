84466e17f1c7a67ab5574c9b86a3d7df2470e3b3
Jonathan Attram <codinghub242@gmail.com>
2026-02-11 01:54:25 +0000

    netr

--- src/app/day-tasks-modal/day-tasks-modal.page.ts
+++ src/app/day-tasks-modal/day-tasks-modal.page.ts
@@ -1,8 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonCheckbox, IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonList, IonItem, IonLabel } from '@ionic/angular/standalone';
+import { IonCheckbox, IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonList, IonItem, IonLabel, IonIcon } from '@ionic/angular/standalone';
 import { IonicModule, ModalController } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { close, calendarOutline, chevronForward } from 'ionicons/icons';
 import { Task } from '../models/task.model';
 import { User } from '../models/user.model';
 
@@ -11,16 +13,33 @@ import { User } from '../models/user.model';
   templateUrl: './day-tasks-modal.page.html',
   styleUrls: ['./day-tasks-modal.page.scss'],
   standalone: true,
-  imports: [IonicModule, IonCheckbox, IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonList, IonItem, IonLabel, CommonModule, FormsModule]
+  imports: [IonicModule, IonCheckbox, IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonList, IonItem, IonLabel, IonIcon, CommonModule, FormsModule]
 })
 export class DayTasksModalPage implements OnInit {
   @Input() selectedDate: Date = new Date();
   @Input() tasks: Task[] = [];
   @Input() users: User[] = [];
 
-  constructor(private modalController: ModalController) { }
+  // Summary stats
+  completedCount: number = 0;
+  pendingCount: number = 0;
+  overdueCount: number = 0;
+
+  constructor(private modalController: ModalController) { 
+    addIcons({close, calendarOutline, chevronForward});
+  }
 
   ngOnInit() {
+    this.calculateSummary();
+  }
+
+  calculateSummary(): void {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+    
+    this.completedCount = this.tasks.filter(t => t.completed).length;
+    this.pendingCount = this.tasks.filter(t => !t.completed).length;
+    this.overdueCount = this.tasks.filter(t => !t.completed && new Date(t.dueDate) < today).length;
   }
 
   getEmployeeName(employeeId?: string): string {
