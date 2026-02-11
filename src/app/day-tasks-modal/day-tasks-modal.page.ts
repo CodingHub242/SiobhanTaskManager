7c53c49812e2af2397abd69c40cf0696e70f9246
Jonathan Attram <codinghub242@gmail.com>
2026-02-11 01:11:47 +0000

    upd

--- src/app/day-tasks-modal/day-tasks-modal.page.ts
+++ src/app/day-tasks-modal/day-tasks-modal.page.ts
@@ -1,20 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonList, IonItem, IonLabel } from '@ionic/angular/standalone';
+import { IonicModule, ModalController } from '@ionic/angular';
+import { Task } from '../models/task.model';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-day-tasks-modal',
   templateUrl: './day-tasks-modal.page.html',
   styleUrls: ['./day-tasks-modal.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonList, IonItem, IonLabel, CommonModule, FormsModule]
 })
 export class DayTasksModalPage implements OnInit {
+  selectedDate: Date = new Date();
+  tasks: Task[] = [];
+  users: User[] = [];
 
-  constructor() { }
+  constructor(private modalController: ModalController) { }
 
   ngOnInit() {
   }
 
+  getEmployeeName(employeeId?: string): string {
+    if (!employeeId) return 'Unassigned';
+    const user = this.users.find(u => u.id === employeeId);
+    return user ? user.name : 'Unassigned';
+  }
+
+  async close(): Promise<void> {
+    await this.modalController.dismiss();
+  }
+
+  async onTaskClick(task: Task): Promise<void> {
+    await this.modalController.dismiss({ task, action: 'view' });
+  }
 }
